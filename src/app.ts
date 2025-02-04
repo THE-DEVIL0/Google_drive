ce83bf5328dbc8ec00117487778dcb443da6692c
Muneeb-ur-Rehman <muneebrajput12466@gmail.com>
2025-02-04 11:28:15 -0800

    added scripts to run the project

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express from "express"
+import express, { response } from "express"
 
 const app = express()
 
-app.set('view engine' , 'ejs' )
+app.set('view engine', 'ejs')
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({extended: true}))
 
 app.use(express.static('public'))
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res)=>{
     res.render('index')
-})
+} )
 
 app.listen(3000, ()=>{
     console.log("Server is running on Port 3000");

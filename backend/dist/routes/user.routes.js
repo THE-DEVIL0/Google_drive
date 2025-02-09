5bf34daf951aae9d64db2502e216b9d6bf411f55
Muneeb-ur-Rehman <muneebrajput12466@gmail.com>
2025-02-09 01:12:02 -0800

    added functionality

--- backend/dist/routes/user.routes.js
+++ backend/dist/routes/user.routes.js
@@ -5,12 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const express_validator_1 = require("express-validator");
 router.get('/', (req, res) => {
     res.render('index');
 });
 router.get('/register', (req, res) => {
     res.render('register');
 });
-router.post('/register', (req, res) => {
+router.post('/register', (0, express_validator_1.body)('username').trim().isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'), (0, express_validator_1.body)('email').trim().isEmail().withMessage('Please enter a valid email address'), (0, express_validator_1.body)('password').trim().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'), (req, res) => {
+    const errors = (0, express_validator_1.validationResult)(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({
+            errors: errors.array(),
+            message: 'Invalid data'
+        });
+        return;
+    }
+    res.status(201).json({ message: 'User created' });
 });
 exports.default = router;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 const router = express.Router()
+import {body, validationResult} from 'express-validator'
 
 
 
@@ -12,7 +13,23 @@ router.get('/register',(req,res)=>{
     res.render('register')
 })
 
-router.post('/register', (req, res)=>{
+router.post('/register',
+body('username').trim().isLength({min: 3}).withMessage('Username must be at least 3 characters long'),  
+body('email').trim().isEmail().withMessage('Please enter a valid email address'), 
+body('password').trim().isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
+(req,res)=>{
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+         res.status(400).json({
+            errors: errors.array(),
+            message: 'Invalid data'
+            
+        })
+        return
+    }
+    res.status(201).json({message: 'User created'})
     
 })
 
